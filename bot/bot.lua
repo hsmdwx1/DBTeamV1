6aca07b2d6bd02586d9f36e2dc4daefda02a6e52
hsmdwx1 <hsmdewx1@gmail.com>
2017-03-23 19:13:56 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {317576202,315017219},
     admin_users = {},
     disabled_channels = {}
   }
